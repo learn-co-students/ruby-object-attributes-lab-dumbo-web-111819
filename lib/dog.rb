0eec25f923e19911eb1d4fef57be0eb8f5adae1e
Baruch Phillips <baruchphillips@gmail.com>
2019-11-19 12:19:58 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    def name=(name_of_dog)
+        @name = name_of_dog
+    end
+    def name
+       @name
+    end 
+    def breed=(name_of_dog)
+        @breed = name_of_dog
+    end 
+    def breed 
+        @breed
+    end 
+end 
+
