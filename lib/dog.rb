41a55223875ea84dc07ebfb8800a78707a4ea6c3
ManateeIdol <benmiriello@gmail.com>
2019-11-19 16:29:01 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name # writer
+    @name
+  end
+
+  def name=(name) # reader
+    @name = name
+  end
+
+  def breed # writer
+    @breed
+  end
+
+  def breed=(breed) # reader
+    @breed = breed
+  end
+
+end # end class Dog
